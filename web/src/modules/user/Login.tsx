c3f611b9a110e93596ee11a1654efd6f1d122fcd
Petra Vos <petravos70@gmail.com>
2018-10-07 17:31:33 +0200

    add folder modules with components

---
+++ web/src/modules/user/Login.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class Login extends React.PureComponent {
+  render() {
+    return <div>login</div>;
+  }
+}
---
+++ web/src/modules/user/TestView.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default class TestView extends React.PureComponent {
+  render() {
+    return <div>test view</div>;
+  }
+}
