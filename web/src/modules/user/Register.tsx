2abee172ffc301e35065ca31b41aa4e1af7bef13
Petra Vos <petravos70@gmail.com>
2018-10-08 19:20:37 +0200

    Add functionality to register. Add apollo-boost

--- web/src/modules/user/Register.tsx
+++ web/src/modules/user/Register.tsx
@@ -1,7 +1,48 @@
 import * as React from "react";
 
 export class Register extends React.PureComponent {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleChange = (e: any) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
   render() {
-    return <div>register</div>;
+    const { password, email } = this.state;
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <div>
+          <input
+            type="text"
+            placeholder="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button onClick={() => console.log("clicked")}>register</button>
+      </div>
+    );
   }
 }
