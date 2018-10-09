78fee29769106eff648b168bb138d7aae9a83892
Petra Vos <petravos70@gmail.com>
2018-10-09 19:38:16 +0200

    refactor: add functionality to file

--- web/src/modules/user/TestView.tsx
+++ web/src/modules/user/TestView.tsx
@@ -1,7 +1,34 @@
 import * as React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+import { TestQuery } from "../../schemaTypes";
+
+const testQuery = gql`
+  query TestQuery {
+    me {
+      id
+      email
+    }
+  }
+`;
 
 export default class TestView extends React.PureComponent {
   render() {
-    return <div>test view</div>;
+    return (
+      <Query<TestQuery> query={testQuery}>
+        {({ data, loading }) => {
+          if (loading) {
+            return null;
+          }
+          if (!data) {
+            return <div>data is undefined</div>;
+          }
+          if (!data.me) {
+            return <div>user not available</div>;
+          }
+          return <div>{data.me.email}</div>;
+        }}
+      </Query>
+    );
   }
 }
