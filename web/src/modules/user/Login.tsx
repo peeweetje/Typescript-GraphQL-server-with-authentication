3f284f1233f01e8131d3b6c6aafc71ba38d9ff24
Petra Vos <petravos70@gmail.com>
2018-10-09 19:37:20 +0200

    refactor: add functionality to file

--- web/src/modules/user/Login.tsx
+++ web/src/modules/user/Login.tsx
@@ -1,7 +1,75 @@
 import * as React from "react";
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
+import { RouteComponentProps } from "react-router-dom";
+import { LoginMutationVariables, LoginMutation } from "../../schemaTypes";
 
-export class Login extends React.PureComponent {
+const loginMutation = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      id
+      email
+    }
+  }
+`;
+
+export class Login extends React.PureComponent<RouteComponentProps<{}>> {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleChange = (e: any) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
   render() {
-    return <div>login</div>;
+    const { password, email } = this.state;
+    return (
+      <Mutation<LoginMutation, LoginMutationVariables> mutation={loginMutation}>
+        {mutate => (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <div>
+              <input
+                type="text"
+                placeholder="email"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                value={password}
+                onChange={this.handleChange}
+              />
+            </div>
+            <button
+              onClick={async () => {
+                const response = await mutate({
+                  variables: this.state
+                });
+                console.log(response);
+                this.props.history.push("/testview");
+              }}
+            >
+              login
+            </button>
+          </div>
+        )}
+      </Mutation>
+    );
   }
 }
