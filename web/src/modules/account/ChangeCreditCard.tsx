6f1ce2de722d79b6e43809bf1cba92a5a72084c2
Petra Vos <petravos70@gmail.com>
2018-10-19 15:04:41 +0200

    Add new file ChangeCreditCard to account folder

---
+++ web/src/modules/account/ChangeCreditCard.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
+import {
+  ChangeCreditCardMutation,
+  ChangeCreditCardMutationVariables
+} from "../../schemaTypes";
+import { userFragment } from "src/graphql/fragments/UserFragment";
+
+const changeCreditCardMutation = gql`
+  mutation ChangeCreditCardMutation($source: String!, $ccLast4: String!) {
+    changeCreditCard(source: $source, ccLast4: $ccLast4) {
+      ...UserInfo
+    }
+  }
+  ${userFragment}
+`;
+
+export class ChangeCreditCard extends React.PureComponent {
+  render() {
+    return (
+      <Mutation<ChangeCreditCardMutation, ChangeCreditCardMutationVariables>
+        mutation={changeCreditCardMutation}
+      >
+        {mutate => (
+          <StripeCheckout
+            token={async token => {
+              const response = await mutate({
+                variables: {
+                  source: token.id,
+                  ccLast4: token.card.last4
+                }
+              });
+              console.log(response);
+            }}
+            stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE!}
+          />
+        )}
+      </Mutation>
+    );
+  }
+}
